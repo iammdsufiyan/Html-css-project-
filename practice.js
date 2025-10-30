130715845b10876a86a9be4ae81a5580366fe578
iammdsufiyan <uaremdsufian@gmail.com>
2025-10-30 13:22:11 +0530

    scope chain

---
+++ practice.js
@@ -0,0 +1,56 @@
+// console.log("practice");
+
+
+// console.log(this===window);
+
+
+// var x = 5;
+
+// console.log(window.location);
+
+// console.log(window.document);
+
+
+
+// console.log(x);
+
+// var x = 7;
+
+// console.log(x);
+
+
+// function a(){
+//     var b = 5;
+//     console.log(b);
+
+// }
+
+// var b = 5;
+
+// a();
+
+
+// function a(){
+//         c();
+
+//     function c (){
+//         console.log(b);
+
+//     }
+// }
+
+// var b = 10;
+// a();
+
+
+function a(){
+     var b = 10;
+
+     c();
+     function c(){
+
+     }
+}
+
+a();
+console.log(b);
