72e22bd0e875967a8d849ad9fe9b32baa635fea5
iammdsufiyan <uaremdsufian@gmail.com>
2026-04-02 12:18:08 +0530

    async

---
+++ namate-javascript/setTimeOut.js
@@ -0,0 +1,15 @@
+// var i = 1;
+// setTimeout(function(){
+//     console.log(i);
+// }, 3000);
+// console.log("md sufiyan");
+
+function x(){
+    for(var i=1; i<=5;i++){
+        setTimeout(function(){
+            console.log(i);
+        }, i*1000);
+    }
+    console.log("hello")
+}
+x();
