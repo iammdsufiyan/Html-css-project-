f023df4b8ae8dfaa6a26b01c991246f6579a53f6
iammdsufiyan <uaremdsufian@gmail.com>
2026-04-05 20:12:56 +0530

    script

--- namate-javascript/setTimeOut.js
+++ namate-javascript/setTimeOut.js
@@ -4,12 +4,22 @@
 // }, 3000);
 // console.log("md sufiyan");
 
-function x(){
-    for(var i=1; i<=5;i++){
-        setTimeout(function(){
-            console.log(i);
-        }, i*1000);
-    }
-    console.log("hello")
+// function x(){
+//     for(var i=1; i<=5;i++){
+//         setTimeout(function(){
+//             console.log(i);
+//         }, i*1000);
+//     }
+//     console.log("hello")
+// }
+// x();
+
+
+const abc = function (){
+    // console.log("function");
 }
-x();
+
+
+const xyz = abc();
+//console.log(abc)
+console.log(xyz)
